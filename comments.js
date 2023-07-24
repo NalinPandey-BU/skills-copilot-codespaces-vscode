2d3441b6d9942d432d1d979f8357047d90a252e4
Ataraxia <79249058+NalinPandey-BU@users.noreply.github.com>
2023-07-24 11:35:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,81 @@
-// create a web server
-// 1. import express
-const express = require('express')
-// 2. create an express application
-const app = express()
-// 3. create a port variable
-const port = 3000
-// 4. import express-handlebars
-const exphbs = require('express-handlebars')
-// 5. import restaurantList.json
-const restaurantList = require('./restaurant.json')
-// 6. setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-// 7. setting static files
-app.use(express.static('public'))
-// 8. import body-parser
-const bodyParser = require('body-parser')
-// 9. setting body-parser
-app.use(bodyParser.urlencoded({ extended: true }))
-// 10. setting routes
-// index
-app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
-})
-// show
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
-})
-// search
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-  })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
-})
-// 11. setting listen
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
-})
+// create a web server// create a route to get all comments
+// create a route to add a comment
+// create a route to delete a comment
+// create a route to update a comment
+// create a route to get a comment by ID
+
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comments');
+
+// GET ALL COMMENTS
+router.get('/', async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// GET ONE COMMENT
+router.get('/:id', getComment, (req, res) => {
+    res.json(res.comment);
+});
+
+// CREATE COMMENT
+router.post('/', async (req, res) => {
+    const comment = new Comment({
+        name: req.body.name,
+        comment: req.body.comment
+    });
+    try {
+        const newComment = await comment.save();
+        res.status(201).json(newComment);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// DELETE COMMENT
+router.delete('/:id', getComment, async (req, res) => {
+    try {
+        await res.comment.remove();
+        res.json({ message: 'Comment deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// UPDATE COMMENT
+router.patch('/:id', getComment, async (req, res) => {
+    if (req.body.name != null) {
+        res.comment.name = req.body.name;
+    }
+    if (req.body.comment != null) {
+        res.comment.comment = req.body.comment;
+    }
+    try {
+        const updatedComment = await res.comment.save();
+        res.json(updatedComment);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Middleware function to get comment by ID
+async function getComment(req, res, next) {
+    try {
+        comment = await Comment.findById(req.params.id);
+        if (comment == null) {
+            return res.status(404).json({ message: 'Cannot find comment' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.comment = comment;
+    next();
+}
+
+module.exports = router;
